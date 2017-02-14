382cfe543d385c32e7a135d21cd7875e2d0a320e
Chantel Duscent <cduscent@claflin.edu>
2017-02-14 10:37:53 -0500

    Created by Coding Ground Client

---
+++ New Project-20170214/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Greetings my friend!!");
+    
+        String nameOfGirl = "Chantel";
+
+        //prompt the user to enter their name
+        System.out.println("Please, enter your name");
+        
+        System.out.println("Hi, you look lovely today " + nameOfGirl);
+      
+     }
+}
